433f6b3e4c57284edfc0125880b83c1e6bd01017
“mbuimbogo” <peter.mbogo@student.moringaschool.com>
2022-08-26 14:04:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+    if (array.length < 2) {
+      return false
+    }
+    for (let i = 0; i < array.length - 1; i++) {
+       for (let j = i + 1; j < array.length; j++) {
+        let sum = array[i] + array[j];
+        if (sum === target) {
+          return true;
+        }
+      }
+    }
+    return false
+  }
+
 
 /* 
   Write the Big O time complexity of your function here
+  Quadratic time: 0(n²) -- since the function has a nested loop
 */
 
 /* 
   Add your pseudocode here
+  initialize an if statement where the length of the array should not be less than 2
+  iterate through the array
+  iterate through the nested array
+  the sum of the elements from iteration one and two  should be equal to target to be true
+  if not so return false
 */
+
 
 /*
   Add written explanation of your solution here
